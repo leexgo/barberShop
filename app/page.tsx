10a2190888f07658833cf119f84d899a14ece58e
leexgo <alex.gomes.dev@gmail.com>
2025-08-14 00:50:35 -0300

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Button } from "./_components/ui/button"
 
 export default function Home() {
+  const name = ""
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
       <h1 className="text-red-500">hello, world</h1>
