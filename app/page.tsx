a467aa2b38f5cd2d3ac4dedf8bd47a124b1b8d9a
leexgo <alex.gomes.dev@gmail.com>
2025-10-17 15:23:56 -0300

    feat: add booking item

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarFallback, AvatarImage } from "./_components/ui/avatar"
 
 export default function Home() {
   return (
@@ -21,12 +24,33 @@ export default function Home() {
 
         <div className="relative mt-6 h-[150px] w-full">
           <Image
+            priority
             alt="Agende com os melhores na wsf Barber"
             src="/banner-01.png"
             fill
             className="rounded-xl object-cover"
           />
         </div>
+        <Card className="mt-6 py-0">
+          <CardContent className="flex justify-between p-0">
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de cabelo</h3>
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://github.com/shadcn.png" />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+                <p className="text-sm">Barbearia FSW</p>
+              </div>
+            </div>
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Fevereiro</p>
+              <p className="text-2xl">8</p>
+              <p className="text-sm">10:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
